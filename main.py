febb82a774c4e94fe63932760e75fec03e3bfae9
F4ruk.seker <ahmetfaruk731@gmail.com>
2023-08-28 01:41:03 +0300

    str list spliter

--- main.py
+++ main.py
@@ -21,6 +21,9 @@ async def question_client_ip(request: Request):
     if len(client_ip) > 1 and type(client_ip) is list:
         client_ip = client_ip[0]
 
+    if len(client_ip.split(',')) > 1:
+        client_ip = client_ip[0]
+
     result = query.get_ip_data(client_ip)
     result['client_ip'] = client_ip
     result = save_query(result)
@@ -39,6 +42,9 @@ async def question_ip(ip: str, request: Request):
 
     if len(result['client_ip']) > 1 and type(result['client_ip']) is list:
         result['client_ip'] = result['client_ip'][0]
+
+    if len(result['client_ip'].split(',')) > 1:
+        result['client_ip'] = result['client_ip'].split(',')[0]
 
     result = save_query(result)
     if SERVER_IP != ip and SERVER_IP != result.get('host'):
